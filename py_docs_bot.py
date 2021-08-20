c36f8b33aa2a33316238a7a92ca47c0cafd8fb18
trevormiller6 <Trevormiller6@gmail.com>
2021-08-19 20:10:16 -0700

    refactoring

--- py_docs_bot.py
+++ py_docs_bot.py
@@ -6,55 +6,70 @@ This reddit bot will monitor the /r/learnpython subreddit and when invoked by ke
 Creator: Trevor Miller
 GitHub: https://github.com/trevormiller6
 reddit: https://www.reddit.com/user/trevor_of_earth/
+Bot's reddit: https://www.reddit.com/user/py_reference_bot
 
 TODO:
  * Remove print() statements and set up actual logging
  * Move to heroku.com or something similar if becomes popular... likely wont be needed...
 '''
 
-# Standard
 import configparser
 import re
 import requests
-
-# Non-standard
 from lxml.html import fromstring
 import praw
+import snoop
 
 
 def main():
     '''
-    Main function
+    Main function to initalize reddit class and authenticate
     '''
 
+    # Try to get bot credentials from the file 'credentials.ini' located in script directory
+    try:
+        config = configparser.ConfigParser()
+        config.read("credentials.ini")
+        reddit_api_id = config["reddit"]["client_id"]
+        reddit_api_secret = config["reddit"]["client_secret"]
+        reddit_username = config["reddit"]["username"]
+        reddit_password = config["reddit"]["password"]
+    except Exception:
+        print("No credentials.ini file found")
+        # Failed to get creds from file so lets check the environment variables.
+        # Only importing the environ method since it will only be needed if
+        # credentials file was not found so we prevent unnecessary imports.
+        from os import environ
+
+        try:
+            reddit_api_id = environ["REDDIT_DOC_BOT_ID"]
+            reddit_api_secret = environ["REDDIT_DOC_BOT_SECRET"]
+            reddit_username = environ["REDDIT_DOC_BOT_USER"]
+            reddit_password = environ["REDDIT_DOC_BOT_PASSWORD"]
+        except KeyError:
+            print("environment variables not found")
+            raise SystemExit
+
+    bot_user_agent = "(praw-python3.9) py_docs_bot - scanning comments in /r/learnpython and replying with python documentation links"
     print("Authenticating to reddit")
     # Instantiate reddit class and authenticate
-    reddit = reddit_authenticate()
-    # Define subreddit to monitor
-    subreddit = reddit.subreddit(subreddit_name)
-    # Call function to start itterating through comments
-    monitor_comments(subreddit)
-
-def reddit_authenticate():
-    '''
-    Authenticate to reddit
-    '''
-
     reddit = praw.Reddit(client_id=reddit_api_id,
                          client_secret=reddit_api_secret,
                          username=reddit_username,
                          password=reddit_password,
                          user_agent=bot_user_agent)
+    # Define subreddit to monitor
+    subreddit = reddit.subreddit("learnpython")
+    # Call function to start itterating through comments
+    monitor_and_reply_to_comments(subreddit)
 
-    return reddit
-
-def monitor_comments(subreddit):
+def monitor_and_reply_to_comments(subreddit):
     '''
     Loop through comments from the learnpython subreddit and check for bot keyword.
     If found parse the topics out and retrive related links to documentation and post a reply with the links.
     '''
 
-    print("Monitoring comments for keyword '!docs'")
+    print("Monitoring r/learnpython comments for keyword '!docs'")
 
     # Loop over comment objects returned from reddit. skip_existing=True means that when the bot
     # starts it will not go back and get existing comments and instead start with new ones.
@@ -65,17 +80,34 @@ def monitor_comments(subreddit):
         # command usage: !docs pathlib.Path, re.search, requests, zip
         if bool(re.search(r"^\!docs.+$", comment.body, flags=re.MULTILINE)):
             needed_references = re.search(r"^\!docs\s(.+)$", comment.body, flags=re.MULTILINE).group(1).replace(" ", "").split(",")
-            all_links = get_links(needed_references)
+            all_links = _get_links(needed_references)
             if all_links:
-                comment.reply(build_comment(all_links))
+                comment.reply(_build_comment(all_links))
                 print("replied to a comment")
 
-def get_links(needed_references):
+def _get_links(needed_references):
     '''
     Gather all the links to reference materials requested.
     '''
 
-    def _validate_link(link):
+    # Create the dictionary that will store all of our links using the python module or search terms as the key.
+    all_links = {}
+    
+    for reference in needed_references:
+
+        ref_link =_get_official_docs(reference)
+        if ref_link:
+            all_links[reference] = ref_link
+
+    return all_links
+
+def _get_official_docs(reference):
+    '''
+    Get link to official python documentation. Python kinda standardized their link structure for their documentation
+    but there is a little weirdness that we check for.
+    '''
+
+    def _is_link_valid():
         '''
         Validate that the links gathered work and if they do format to markdown 
         with the actual page name else return an empty string.
@@ -89,61 +121,42 @@ def get_links(needed_references):
         else:
 
             return False
-    
-    def _get_official_docs(reference):
-        '''
-        Get link to official python documentation. Python kinda standardized their link structure for their documentation
-        but there is a little weirdness that we check for.
-        '''
 
-        builtin_functions = ["abs", "delattr", "hash", "memoryview", "set", "all", "dict", "help", "min", "setattr", "any", "dir", "hex", "next", "slice", "ascii", 
-                            "divmod", "id", "object", "sorted", "bin", "enumerate", "input", "oct", "staticmethod", "bool", "eval", "int", "open", "str", "breakpoint", 
-                            "exec", "isinstance", "ord", "sum", "bytearray", "filter", "issubclass", "pow", "super", "bytes", "float", "iter", "print", "tuple", "callable", 
-                            "format", "len", "property", "type", "chr", "frozenset", "list", "range", "vars", "classmethod", "getattr", "locals", "repr", "zip", "compile", 
-                            "globals", "map", "reversed", "__import__", "complex", "hasattr", "max", "round"]
+    builtin_functions = ["abs", "delattr", "hash", "memoryview", "set", "all", "dict", "help", "min", "setattr", "any", "dir", "hex", "next", "slice", "ascii", 
+                        "divmod", "id", "object", "sorted", "bin", "enumerate", "input", "oct", "staticmethod", "bool", "eval", "int", "open", "str", "breakpoint", 
+                        "exec", "isinstance", "ord", "sum", "bytearray", "filter", "issubclass", "pow", "super", "bytes", "float", "iter", "print", "tuple", "callable", 
+                        "format", "len", "property", "type", "chr", "frozenset", "list", "range", "vars", "classmethod", "getattr", "locals", "repr", "zip", "compile", 
+                        "globals", "map", "reversed", "__import__", "complex", "hasattr", "max", "round"]
 
-        # For python built-in functions (zip, map, filter, enumerate, etc.), they did not get their own page and instead are all on one page.
-        if reference in builtin_functions:
-            link = f"https://docs.python.org/3/library/functions.html#{reference}"
-            is_valid_link = _validate_link(link)
-        # If function was not in the module name then attempt to create a link with the full module name, ex. `pathlib.Path`.
-        # This serves 2 purposes: first to accomadate modules names that don't include class, and second for things like `os.path`
-        # that for some reason has its own page in the docs seprate from the `os` docs.
-        else:
-            link = f"https://docs.python.org/3/library/{reference}.html#{reference}"
-            is_valid_link = _validate_link(link)
+    # For python built-in functions (zip, map, filter, enumerate, etc.), they did not get their own page and instead are all on one page.
+    if reference in builtin_functions:
+        link = f"https://docs.python.org/3/library/functions.html#{reference}"
+    # If function was not in the module name then attempt to create a link with the full module name, ex. `pathlib.Path`.
+    # This serves 2 purposes: first to accomadate modules names that don't include class, and second for things like `os.path`
+    # that for some reason has its own page in the docs seprate from the `os` docs.
+    else:
+        link = f"https://docs.python.org/3/library/{reference}.html#{reference}"
 
-        if is_valid_link:
+    if _is_link_valid():
+
+        return link
+
+    # If after testing the above links to python documentation fails than there is one last url path to try.
+    # This is actually the url path that most of the documentation will have.
+    # Split on the `.` and grab the first item in the list which will be the library name ex. pathlib.Path becomes just pathlib
+    else:
+        link = f"https://docs.python.org/3/library/{reference.split('.')[0]}.html#{reference}"
+
+        if _is_link_valid():
 
             return link
-        # If after testing the above links to python documentation fails than there is one last url path to try.
-        # This is actually the url path that most of the documentation will have.
-        # Split on the `.` and grab the first item in the list which will be the library name ex. pathlib.Path becomes just pathlib
+
+        # If all of the above failed then it most likely is not a python standard library or function or the user had a typo.
         else:
-            link = f"https://docs.python.org/3/library/{reference.split('.')[0]}.html#{reference}"
-            is_valid_link = _validate_link(link)
 
-            if is_valid_link:
+            return False
 
-                return link
-
-            # If all of the above failed then it most likely is not a python standard library or function or the user had a typo.
-            else:
-
-                return False
-
-    # Create the dictionary that will store all of our links using the python module or search terms as the key.
-    all_links = {}
-    
-    for reference in needed_references:
-
-        ref_link =_get_official_docs(reference)
-        if ref_link:
-            all_links[reference] = ref_link
-
-    return all_links
-
-def build_comment(all_links):
+def _build_comment(all_links):
     '''
     Build the comment that will have all of the reference links.
     Format in markdown.
@@ -157,28 +170,18 @@ def build_comment(all_links):
 
         comment_markdown += f"[{reference} - {link}]({link}){new_line}"
 
-    comment_markdown += f"{new_line}Python Reference Bot - *[GitHub](https://github.com/trevormiller6/Py-Docs-Bot)*"
+    comment_markdown += f"{new_line}Python Documentation Bot - *[GitHub](https://github.com/trevormiller6/Py-Docs-Bot)*"
 
     return comment_markdown
 
-
 if __name__ == "__main__":
-
-    config = configparser.ConfigParser()
-    config.read("credentials.ini")
-    reddit_api_id = config["reddit"]["client_id"]
-    reddit_api_secret = config["reddit"]["client_secret"]
-    reddit_username = config["reddit"]["username"]
-    reddit_password = config["reddit"]["password"]
-    subreddit_name = "learnpython"
-    bot_user_agent = "(praw-python3.9) py_docs_bot - scanning comments in /r/learnpython and replying with python documentation links"
-
-    while True:
-        try:
-            main()
-        except Exception as e:
-            print(e)
-            continue
-        except KeyboardInterrupt:
-            print("Exiting")
-            break
+    main()
+    # while True:
+    #     try:
+    #         main()
+    #     except Exception as e:
+    #         print(e)
+    #         continue
+    #     except (KeyboardInterrupt, SystemExit):
+    #         print("Exiting")
+    #         raise SystemExit
