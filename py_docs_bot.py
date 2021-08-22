7fae3bae0d5b7f8b8d88cbabfce073a9cfc36170
trevormiller6 <Trevormiller6@gmail.com>
2021-08-21 17:22:00 -0700

    Added feature to return links to python reference documentation.

--- py_docs_bot.py
+++ py_docs_bot.py
@@ -14,9 +14,11 @@ TODO:
 '''
 
 import configparser
+from json import loads
 from os import environ
 import re
 import requests
+from fuzzywuzzy import fuzz
 import praw
 
 
@@ -91,68 +93,103 @@ def _get_links(needed_references):
 
     for reference in needed_references:
 
-        ref_link =_get_official_docs(reference)
+        REFLINKS = [link for link in _get_official_docs(reference) if link ]
 
-        if ref_link:
-            all_links[reference] = ref_link
+        if REFLINKS:
+            all_links[reference] = REFLINKS
 
     return all_links
 
 def _get_official_docs(reference):
     '''
-    Get link to official python documentation. Python kinda standardized their link structure for their documentation
-    but there is a little weirdness that we check for.
+    Get link to official python documentation. 
     '''
 
-    def _is_link_valid():
+    def _standard_library():
         '''
-        Validate that the links gathered work and if they do format to markdown 
-        with the actual page name else return an empty string.
+        Get links to the documentation on the standard library. 
+        Python kinda standardized their link structure for their documentation 
+        but there is a little weirdness that we check for.
         '''
 
-        link_results = requests.get(link)
+        def _is_link_valid():
+            '''
+            Validate that the links gathered work and if they do format to markdown 
+            with the actual page name else return an empty string.
+            '''
 
-        if link_results:
+            link_results = requests.get(link)
 
-            return True
+            if link_results:
 
+                return True
+
+            else:
+
+                return False
+
+        builtin_functions = ["abs", "delattr", "hash", "memoryview", "set", "all", "dict", "help", "min", "setattr", "any", "dir", "hex", "next", "slice", "ascii", 
+                            "divmod", "id", "object", "sorted", "bin", "enumerate", "input", "oct", "staticmethod", "bool", "eval", "int", "open", "str", "breakpoint", 
+                            "exec", "isinstance", "ord", "sum", "bytearray", "filter", "issubclass", "pow", "super", "bytes", "float", "iter", "print", "tuple", "callable", 
+                            "format", "len", "property", "type", "chr", "frozenset", "list", "range", "vars", "classmethod", "getattr", "locals", "repr", "zip", "compile", 
+                            "globals", "map", "reversed", "__import__", "complex", "hasattr", "max", "round"]
+
+        # For python built-in functions (zip, map, filter, enumerate, etc.), they did not get their own page and instead are all on one page.
+        if reference in builtin_functions:
+            link = f"https://docs.python.org/3/library/functions.html#{reference}"
+        # If function was not in the module name then attempt to create a link with the full module name, ex. `pathlib.Path`.
+        # This serves 2 purposes: first to accomadate modules names that don't include class, and second for things like `os.path`
+        # that for some reason has its own page in the docs seprate from the `os` docs.
         else:
-
-            return False
-
-    builtin_functions = ["abs", "delattr", "hash", "memoryview", "set", "all", "dict", "help", "min", "setattr", "any", "dir", "hex", "next", "slice", "ascii", 
-                        "divmod", "id", "object", "sorted", "bin", "enumerate", "input", "oct", "staticmethod", "bool", "eval", "int", "open", "str", "breakpoint", 
-                        "exec", "isinstance", "ord", "sum", "bytearray", "filter", "issubclass", "pow", "super", "bytes", "float", "iter", "print", "tuple", "callable", 
-                        "format", "len", "property", "type", "chr", "frozenset", "list", "range", "vars", "classmethod", "getattr", "locals", "repr", "zip", "compile", 
-                        "globals", "map", "reversed", "__import__", "complex", "hasattr", "max", "round"]
-
-    # For python built-in functions (zip, map, filter, enumerate, etc.), they did not get their own page and instead are all on one page.
-    if reference in builtin_functions:
-        link = f"https://docs.python.org/3/library/functions.html#{reference}"
-    # If function was not in the module name then attempt to create a link with the full module name, ex. `pathlib.Path`.
-    # This serves 2 purposes: first to accomadate modules names that don't include class, and second for things like `os.path`
-    # that for some reason has its own page in the docs seprate from the `os` docs.
-    else:
-        link = f"https://docs.python.org/3/library/{reference}.html#{reference}"
-
-    if _is_link_valid():
-
-        return link
-
-    # If after testing the above links to python documentation fails than there is one last url path to try.
-    # This is actually the url path that most of the documentation will have.
-    # Split on the `.` and grab the first item in the list which will be the library name ex. pathlib.Path becomes just pathlib
-    else:
-        link = f"https://docs.python.org/3/library/{reference.split('.')[0]}.html#{reference}"
+            link = f"https://docs.python.org/3/library/{reference}.html#{reference}"
 
         if _is_link_valid():
 
-            return link
+            return f"[{reference} - {link}]({link})  \n"
 
-        # If all of the above failed then it most likely is not a python standard library or function or the user had a typo.
+        # If after testing the above links to python documentation fails than there is one last url path to try.
+        # This is actually the url path that most of the documentation will have.
+        # Split on the `.` and grab the first item in the list which will be the library name ex. pathlib.Path becomes just pathlib
+        else:
+            link = f"https://docs.python.org/3/library/{reference.split('.')[0]}.html#{reference}"
+
+            if _is_link_valid():
+
+                return f"[{reference} - {link}]({link})  \n"
+
+            # If all of the above failed then it most likely is not a python standard library or function or the user had a typo.
+            else:
+
+                return False
+
+    def _reference_docs():
+        '''
+        Get links to reference documentation from the python docs site.
+        I use fuzzy searching here so that docs called up without having to know the actual title of the reference
+        that is being requested. Requires a minimum match score of 85. May need to tweak this number... not sure yet.
+        '''
+
+        matched_references = []
+
+        for link_dict in REFLINKS["python_ref_docs_url_data"]:
+
+            match_ratio = fuzz.token_set_ratio(link_dict["title"], reference)
+
+            if match_ratio > 85:
+                matched_references.append(f'[{link_dict["title"]} - {REFLINKS["python_ref_docs_base_url"]}{link_dict["link"]}]({REFLINKS["python_ref_docs_base_url"]}{link_dict["link"]})  \n')
+
+        if matched_references:
+
+            return matched_references
+
         else:
 
-            return False
+            return []
+
+    combined_links = [_standard_library(), *_reference_docs()]
+    
+    return combined_links
+    
 
 def _build_comment(all_links):
     '''
@@ -160,17 +197,24 @@ def _build_comment(all_links):
     Format in markdown.
     '''
 
-    new_line = "  \n"
     comment_markdown = ""
 
-    for reference, link in all_links.items():
-        comment_markdown += f"[{reference} - {link}]({link}){new_line}"
+    for links in all_links.values():
+        if isinstance(links, list):
+            for link in links:
+                comment_markdown += link
+        else:
+            comment_markdown += link
 
-    comment_markdown += f"{new_line}Python Documentation Bot - *[GitHub](https://github.com/trevormiller6/Py-Docs-Bot)*"
+    comment_markdown += f"  \nPython Documentation Bot - *[How To Use](https://github.com/trevormiller6/Py-Docs-Bot)*"
 
     return comment_markdown
 
+
 if __name__ == "__main__":
+
+    with open("python_reference_docs.json", "r") as file:
+        REFLINKS = loads(file.read())
 
     while True:
         try:
